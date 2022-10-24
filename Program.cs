011691b42fa38085a2d2bcbe326f3e7fb508e3c2
BÜŞRA OZAN KARAOZAN <95549258+karaozan09@users.noreply.github.com>
2022-10-24 20:04:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haftaucaltıncıkısım
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Derece degerini giriniz");
+            int derece = Convert.ToInt32(Console.ReadLine());
+            double radyan = derece * Math.PI / 180;
+            double gradyan = derece * 200 / 180;
+            Console.WriteLine("derecenın radyan degeri=" + radyan);
+            Console.WriteLine("derecenin gradyan degeri=" + gradyan);
+            Console.ReadKey();
+        }
+    }
+}
